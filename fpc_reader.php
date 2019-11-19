8ffd1f4285471306e5eda53736eb1ca35dd968bf
Valentyn Tykhomyrov <khomyart@gmail.com>
2019-11-19 16:59:07 +0200

    Some major changes

---
+++ fpc_reader.php
@@ -0,0 +1,35 @@
+<?php
+    include "header.php";
+
+    $words_devider="**|||";
+    $string = file_get_contents("fpc_data.dat");
+    $string = explode ($words_devider, $string);
+
+    var_dump($string);
+
+    include "footer.php";
+    
+    $description = [
+        "title" => "Title",
+        "sdesc" => "Short Description",
+        "desc" => "Description"
+    ];
+
+    $restrictions = [
+        "minprice" => "Min. price",
+        "minstay" => "Min. stay",
+        "maxstay" => "Max. stay",
+        "cancelpolicy" => "Cancelation policy"
+    ];
+
+    $wifi = [
+        "network" => "Network",
+        "password" => "Password"
+    ];
+
+    $ownerdata = [
+        "mail" => "Email",
+        "phone" => "Phone"
+    ]
+    
+?>
